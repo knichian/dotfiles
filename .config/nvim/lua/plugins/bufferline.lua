9597df69ae547e32ee758cb357f10b59329919ef
Gabriel <knichian@gmail.com>
2025-09-21 21:51:17 -0300

    editing bufferline.lua configuration on neovim

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -2,5 +2,9 @@ return {
 	'akinsho/bufferline.nvim', 
 	version = "*", 
 	dependencies = 'nvim-tree/nvim-web-devicons',
-	opts = {},
+	opts = {
+		options = {
+			separator_style = 'slant'
+		}
+	},
 }
