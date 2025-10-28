693fc05ff7e8945b25910629bd4ddb9670936b35
Gabriel <knichian@gmail.com>
2025-10-28 11:50:48 -0300

    Adding keymap for blink on nvim

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -25,7 +25,12 @@ return {
     -- C-k: Toggle signature help (if signature.enabled = true)
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = 'default' },
+    keymap = {
+		preset = 'default',
+		['<C-j>'] = { 'accept', 'fallback' },
+		['<CR>'] = { 'accept', 'fallback' },
+		['<C-c>'] = { 'cancel', 'fallback' },
+	},
 
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
