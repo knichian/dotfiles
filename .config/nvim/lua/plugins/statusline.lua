24bb43bfeee081e83045ae332ba4ef4dedb1ecfb
Gabriel <knichian@gmail.com>
2025-10-28 23:36:03 -0300

    nvim/plugins: merging statusline plugings files

--- .config/nvim/lua/plugins/statusline.lua
+++ .config/nvim/lua/plugins/statusline.lua
@@ -1,6 +1,16 @@
 return {
-	'echasnovski/mini.statusline',
-	enabled = false,
-	version = false,
-	opts = {},
+	{
+		'nvim-lualine/lualine.nvim',
+		enabled = true,
+		lazy = false,
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		opts = {},
+	},
+	{
+		'echasnovski/mini.statusline',
+		enabled = false,
+		lazy = false,
+		version = false,
+		opts = {},
+	}
 }
