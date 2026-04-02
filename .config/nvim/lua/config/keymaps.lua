6ab0bf9e8bcfe20198c5adcf94dc4f195f20a110
Gabriel César <knichian@gmail.com>
2026-04-01 21:10:26 -0300

    refactor: reorganizando keymaps no neovim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,49 +1,31 @@
 
--- local keymap = function(mode, lhs, rhs, opts)
--- 	local base_opts = { noremap = true, silent = true }
--- 	opts = vim.tbl_deep_extend('force', base_opts, opts)
--- 	vim.keymap.set(mode, lhs, rhs, opts)
--- end
-
 local base_opts = { noremap = true, silent = true }
 local keymap = vim.keymap.set
 
--- local cmd = function(str)
--- 	local cmd_str = '<Cmd>' .. str .. '<CR>'
--- 	return cmd_str
--- end
-
 local cmd = function(str)
-	return '<Cmd>' .. str .. '<CR>'
+	return "<Cmd>" .. str .. "<CR>"
 end
 
 -- using LSP defaults
 
--- Normal --
--- Remap CTRL-c to a perfect ESC
-keymap('', '<C-c>', '<Esc>', base_opts)
+-- All-Modes -- 
+keymap("", "<C-c>", "<Esc>", base_opts) -- Remap CTRL-c to ESC on all modes
+keymap("", "<C-c>", cmd("nohlsearch"), base_opts) -- switch off highlight from last search
+keymap("", "<Esc>", cmd("nohlsearch"), base_opts) -- switch off highlight from last search
 
--- File Fuzzyfinder 
-keymap('n', '<leader>ff', cmd('Telescope find_files'), base_opts)
+-- Normal --
+keymap("n", "<leader>bd", cmd("bd"), base_opts) -- Close current buffer
+keymap("n", "<Tab>", cmd("BufferLineCycleNext"), base_opts) -- Cycle tabs forth
+keymap("n", "<S-Tab>", cmd("BufferLineCyclePrev"), base_opts) -- Cycle tabs back
+keymap("n", "<C-w>o", cmd("only"), base_opts) -- Makes the current focussed pane the only pane
+keymap("n", "<leader>e", cmd("Lexplore 30"), base_opts) -- Toggle quick file manager
+keymap("n", "<leader>ff", cmd("Telescope find_files"), base_opts) -- Open File Fuzzyfinder 
 
 -- Visual --
--- Stay in indent mode:
-keymap("v", "<", "<gv", base_opts)
-keymap("v", ">", ">gv", base_opts)
+keymap("v", "<", "<gv", base_opts) -- Preserve selection while indenting
+keymap("v", "<Esc>", "v", base_opts) -- Makes Control-c quit directly to Normal mode
+keymap("v", "<C-c>", "v", base_opts) -- Makes Control-c quit directly to Normal mode
 
--- switch off highlight from last search:
-keymap("", "<C-c>", cmd('nohlsearch'), base_opts)
-keymap("", "<Esc>", cmd('nohlsearch'), base_opts)
-
--- Cycle tabs back and forth
-keymap("n", "<Tab>", cmd("BufferLineCycleNext"), base_opts)
-keymap("n", "<S-Tab>", cmd("BufferLineCyclePrev"), base_opts)
-
--- Close current buffer
-keymap('n', '<leader>bd', cmd('bd'), base_opts)
-
--- Only window
-keymap('n', '<C-w>o', cmd('only'), base_opts)
-
-keymap('n', '<leader>e', cmd('Lexplore 30'), base_opts) -- quick access to file manager
+-- Insert -- 
+keymap("i", "<C-c>", "<Esc>", base_opts) -- Makes Control-c quit directly to Normal mode
 
